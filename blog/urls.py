74734cea79ee5218eb957b149b341f45d3c40d93
GennadiiTurutin <gennadii.turutin@gmail.com>
2018-09-12 23:00:34 -0400

    Rearranged

---
+++ blog/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .views import (
+	PostListView, 
+	PostDetailView,
+	PostCreateView,
+	PostUpdateView,
+	PostDeleteView
+	)
+
+from . import views
+
+urlpatterns = [
+    path('', views.me, name='blog-me'),
+    path('blog', PostListView.as_view(), name='blog-blog'),
+    path('gallery/', views.gallery, name='blog-gallery'), 
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'), 
+
+    ]
+
+
+urlpatterns = [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
